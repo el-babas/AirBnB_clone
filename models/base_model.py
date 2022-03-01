071a95779062c393ba27b525345a7cd0941cfd1a
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-01 15:58:41 -0500

    [Modify] Base Model  Changes to be committed: 	modified:   models/base_model.py 	modified:   shell_script/executable_files.sh 	modified:   shell_script/style_code.sh 	new file:   test_base_model.py  Changes in file base_model.py 	new method: to_dict(); save()

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class BaseModel:
     """ Class:
             BaseModel that defines all common attributes/methods
-            for other classes
+            for other classes.
     """
 
     def __init__(self):
@@ -22,13 +22,35 @@ class BaseModel:
 
     def __str__(self):
         """ Method (replace):
-                Represents the class objects as a string in format
+                Represents the class objects as a string in format.
             Return:
-                String in  format '[<class name>] (<self.id>) <self.__dict__>'
+                String in  format '[<class name>] (<self.id>) <self.__dict__>'.
         """
         class_str = ""
         class_str += "[" + self.__class__.__name__ + "] "
         class_str += "(" + self.id + ") "
         # note! : need to implement
-        class_str += self.__dict__
+        class_str += str(self.__dict__)
         return class_str
+
+    def save(self):
+        """ Method:
+                Update auditory date and save change.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Method:
+                Generate dictionary of the class.
+            Return:
+                A dictionary containing all keys/values
+                of __dict__ of the instance.
+        """
+        class_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in ["created_at", "updated_at"]:
+                class_dict[key] = value.isoformat()
+            elif value:
+                class_dict[key] = value
+        class_dict["__class__"] = self.__class__.__name__
+        return class_dict
