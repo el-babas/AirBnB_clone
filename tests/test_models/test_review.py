23c3fb065790c131838259ff5dc8abdee0a3d896
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-06 17:05:24 -0500

    Tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,18 +1,101 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Class definition
+Unittests for Review.
 """
+import unittest
+import pycodestyle
+import datetime
+import models
 from models.base_model import BaseModel
 from models.review import Review
-import unittest
 
 
-class TestModules(unittest.TestCase):
+class CodeStyleTests(unittest.TestCase):
     """
-    Class definition
+    Code style test.
     """
-    def test_uper(self):
+
+    def testPycodestyle(self):
         """
-        Method definition
+        Tests cases for pycodestyle.
         """
-        self.assertEqual(5, 3)
+        code_style = pycodestyle.StyleGuide(quiet=True)
+        result = code_style.check_files(["models/review.py"])
+        self.assertEqual(result.total_errors, 0, "Review pycodestyle error")
+
+
+class DocumentationTests(unittest.TestCase):
+    """
+    Documentation test.
+    """
+
+    def testDocString(self):
+        """
+        Tests cases for docstring.
+        """
+        self.assertIsNotNone(models.review.__doc__,
+                             "Review not docstring")
+        self.assertTrue(len(__doc__) > 0, "Review not docstring")
+        self.assertFalse(len(__doc__) < 0, "Review not docstring")
+
+
+class ReviewTests(unittest.TestCase):
+    """
+    Review tests.
+    """
+    rw = Review()
+
+    def testAttributes(self):
+        """
+        Test cases for the attributes.
+        """
+        self.rw.place_id = "Place.id"
+        self.rw.user_id = "User.id"
+        self.rw.text = "New Review"
+        dict_am = self.rw.to_dict()
+
+        self.assertEqual(self.rw.place_id, dict_am["place_id"])
+        self.assertEqual(self.rw.user_id, dict_am["user_id"])
+        self.assertEqual(self.rw.text, dict_am["text"])
+        self.assertEqual("Review", dict_am["__class__"])
+
+    def testInheritance(self):
+        """
+        Test cases verify is subclass of BaseModel
+        """
+        self.assertIsInstance(self.rw, BaseModel)
+        self.assertTrue(hasattr(self.rw, 'id'))
+        self.assertTrue(hasattr(self.rw, 'created_at'))
+        self.assertTrue(hasattr(self.rw, 'updated_at'))
+
+    def testTypeAttributes(self):
+        """
+        Test case for validate type for attributes.
+        """
+        self.assertIsInstance(self.rw.place_id, str)
+        self.assertIsInstance(self.rw.user_id, str)
+        self.assertIsInstance(self.rw.text, str)
+        self.assertIsInstance(self.rw.id, str)
+        self.assertIsInstance(self.rw.created_at, datetime.datetime)
+        self.assertIsInstance(self.rw.updated_at, datetime.datetime)
+
+    def testDateUpdate(self):
+        """
+        Test case for validate change update_at.
+        """
+        self.rw.text = "Update 1"
+        self.rw.save()
+        dict_1 = self.rw.to_dict()
+
+        self.rw.text = "Update 2"
+        self.rw.save()
+        dict_2 = self.rw.to_dict()
+
+        self.assertEqual(dict_1["id"], dict_2["id"])
+        self.assertEqual(dict_1["created_at"], dict_2["created_at"])
+        self.assertNotEqual(dict_1["updated_at"], dict_2["updated_at"])
+        self.assertNotEqual(dict_1["text"], dict_2["text"])
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,18 +1,95 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Class definition
+Unittests for State.
 """
+import unittest
+import pycodestyle
+import datetime
+import models
 from models.base_model import BaseModel
 from models.state import State
-import unittest
 
 
-class TestModules(unittest.TestCase):
+class CodeStyleTests(unittest.TestCase):
     """
-    Class definition
+    Code style test.
     """
-    def test_uper(self):
+
+    def testPycodestyle(self):
         """
-        Method definition
+        Tests cases for pycodestyle.
         """
-        self.assertEqual(5, 3)
+        code_style = pycodestyle.StyleGuide(quiet=True)
+        result = code_style.check_files(["models/state.py"])
+        self.assertEqual(result.total_errors, 0, "State pycodestyle error")
+
+
+class DocumentationTests(unittest.TestCase):
+    """
+    Documentation test.
+    """
+
+    def testDocString(self):
+        """
+        Tests cases for docstring.
+        """
+        self.assertIsNotNone(models.state.__doc__,
+                             "State not docstring")
+        self.assertTrue(len(__doc__) > 0, "State not docstring")
+        self.assertFalse(len(__doc__) < 0, "State not docstring")
+
+
+class StateTests(unittest.TestCase):
+    """
+    State tests.
+    """
+    st = State()
+
+    def testAttributes(self):
+        """
+        Test cases for the attributes.
+        """
+        self.st.name = "New State"
+        dict_am = self.st.to_dict()
+
+        self.assertEqual(self.st.name, dict_am["name"])
+        self.assertEqual("State", dict_am["__class__"])
+
+    def testInheritance(self):
+        """
+        Test cases verify is subclass of BaseModel
+        """
+        self.assertIsInstance(self.st, BaseModel)
+        self.assertTrue(hasattr(self.st, 'id'))
+        self.assertTrue(hasattr(self.st, 'created_at'))
+        self.assertTrue(hasattr(self.st, 'updated_at'))
+
+    def testTypeAttributes(self):
+        """
+        Test case for validate type for attributes.
+        """
+        self.assertIsInstance(self.st.name, str)
+        self.assertIsInstance(self.st.id, str)
+        self.assertIsInstance(self.st.created_at, datetime.datetime)
+        self.assertIsInstance(self.st.updated_at, datetime.datetime)
+
+    def testDateUpdate(self):
+        """
+        Test case for validate change update_at.
+        """
+        self.st.name = "Update 1"
+        self.st.save()
+        dict_1 = self.st.to_dict()
+
+        self.st.name = "Update 2"
+        self.st.save()
+        dict_2 = self.st.to_dict()
+
+        self.assertEqual(dict_1["id"], dict_2["id"])
+        self.assertEqual(dict_1["created_at"], dict_2["created_at"])
+        self.assertNotEqual(dict_1["updated_at"], dict_2["updated_at"])
+        self.assertNotEqual(dict_1["name"], dict_2["name"])
+
+
+if __name__ == "__main__":
+    unittest.main()
