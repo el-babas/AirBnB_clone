08ef18538e5fb8fbcc59b8d5a45ed3e660a1f24d
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-06 13:21:34 -0500

    [Add] Test models  Changes to be committed: 	new file:   tests/test_models/test_amenity.py 	new file:   tests/test_models/test_city.py 	new file:   tests/test_models/test_place.py 	new file:   tests/test_models/test_review.py 	new file:   tests/test_models/test_state.py 	new file:   tests/test_models/test_user.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""
+Unittests for City.
+"""
+import unittest
+import pycodestyle
+import datetime
+import models
+from models.base_model import BaseModel
+from models.city import City
+
+
+class CodeStyleTests(unittest.TestCase):
+    """
+    Code style test.
+    """
+
+    def testPycodestyle(self):
+        """
+        Tests cases for pycodestyle.
+        """
+        code_style = pycodestyle.StyleGuide(quiet=True)
+        result = code_style.check_files(["models/city.py"])
+        self.assertEqual(result.total_errors, 0, "City pycodestyle error")
+
+
+class DocumentationTests(unittest.TestCase):
+    """
+    Documentation test.
+    """
+
+    def testDocString(self):
+        """
+        Tests cases for docstring.
+        """
+        self.assertIsNotNone(models.city.__doc__,
+                             "City not docstring")
+        self.assertTrue(len(__doc__) > 0, "City not docstring")
+        self.assertFalse(len(__doc__) < 0, "City not docstring")
+
+
+class CityTests(unittest.TestCase):
+    """
+    BaseModel tests.
+    """
+    ct = City()
+
+    def testAttributes(self):
+        """
+        Test cases for the attributes.
+        """
+        self.ct.name = "New City"
+        self.ct.state_id = "State.id"
+        dict_am = self.ct.to_dict()
+
+        self.assertEqual(self.ct.name, dict_am["name"])
+        self.assertEqual(self.ct.state_id, dict_am["state_id"])
+        self.assertEqual("City", dict_am["__class__"])
+
+    def testInheritance(self):
+        """
+        Test cases verify is subclass of BaseModel
+        """
+        self.assertIsInstance(self.ct, BaseModel)
+        self.assertTrue(hasattr(self.ct, 'id'))
+        self.assertTrue(hasattr(self.ct, 'created_at'))
+        self.assertTrue(hasattr(self.ct, 'updated_at'))
+
+    def testTypeAttributes(self):
+        """
+        Test case for validate type for attributes.
+        """
+        self.assertIsInstance(self.ct.name, str)
+        self.assertIsInstance(self.ct.state_id, str)
+        self.assertIsInstance(self.ct.id, str)
+        self.assertIsInstance(self.ct.created_at, datetime.datetime)
+        self.assertIsInstance(self.ct.updated_at, datetime.datetime)
+
+    def testDateUpdate(self):
+        """
+        Test case for validate change update_at.
+        """
+        self.ct.name = "Update 1"
+        self.ct.save()
+        dict_1 = self.ct.to_dict()
+
+        self.ct.name = "Update 2"
+        self.ct.save()
+        dict_2 = self.ct.to_dict()
+
+        self.assertEqual(dict_1["id"], dict_2["id"])
+        self.assertEqual(dict_1["created_at"], dict_2["created_at"])
+        self.assertNotEqual(dict_1["updated_at"], dict_2["updated_at"])
+        self.assertNotEqual(dict_1["name"], dict_2["name"])
+
+
+if __name__ == "__main__":
+    unittest.main()
